82f082514a335fb0b88ae4cd966d3b1bcdfdc89c
Nila Selvaraj <nila.selvaraj@utexas.edu>
2020-06-07 16:01:53 -0500

    join page - added image

--- src/Join.js
+++ src/Join.js
@@ -6,12 +6,19 @@ export default class Join extends Component {
     const jointext = "We're looking for committed UT students to help us make our team even better. Whether you're interested in gaining real experience creating content for the materials team, or enhancing your speech skills with the presentations team, we'd love to have you!";
     return (
       <div>
-        <div className="join">
-          <h1>INTERESTED IN JOINING?</h1>
-          <p>{jointext}</p>
-          <a className="join-button" href="https://www.facebook.com/groups/549689365148387/events/">
-            Join the Facebook group to see our upcoming events
-          </a>
+        <div className="flex join">
+          <div>
+            <img src="/images/weareroadshow.png"/>
+          </div>
+
+          <div id="join-block">
+            <h1>INTERESTED IN JOINING?</h1>
+            <p>{jointext}</p>
+            <a className="join-button" href="https://www.facebook.com/groups/549689365148387/events/">
+              Join the Facebook group to see our upcoming events
+            </a>
+          </div>
+
         </div>
       </div>
       
