c48eba6bbaf5e13de7da1ebad74512fd1616f5ff
Nila Selvaraj <nila.selvaraj@utexas.edu>
2020-06-07 15:32:14 -0500

    updated about page icons

--- src/About.js
+++ src/About.js
@@ -5,14 +5,14 @@ import './style.css';
 export default class About extends Component {
   render() {
     let whatwedo = "We go to local K-12 schools and give a thirty-minute to one-hour presentation to expose students to what Computer Science is, why Computer Science is important, and how they can prepare for a career within Computer Science. We have presentations for students of all ages, and we want to get students excited about Computer Science."
-    let whatiscs = "We introduce the concept of Computer Science to kids with our presentation and with a group activity."
-    let whycs = "Computer Science teaches logical thinking and methodical problem solving. Having tech skills can open up many future opportunities, and it's also lots of fun."
+    let whatiscs = "We introduce and define the concept of Computer Science to students with our presentation and with a group activity, with time for Q&A."
+    let whycs = "Computer Science teaches logical thinking and methodical problem solving. Having tech skills can open up future opportunities, and it's also lots of fun."
     let howtoprepare = "We also introduce local programs and camps that students can attend, and talk about what classes they can take before their college career."
     return (
       <div>
         <div className={"about"}>
           <h1>WHAT WE DO</h1>
-          <p id="what-we-do">{whatwedo}</p>
+          <p id="what-we-do-summary">{whatwedo}</p>
           <div className={"flex about-text"}>
             <div>
               <img src="images/hello_world.png"/>
@@ -25,7 +25,7 @@ export default class About extends Component {
               <p>{whycs}</p>
             </div>
             <div>
-              <img src="images/play.png"/>
+              <img src="images/robot.png"/>
               <h2>How to prepare?</h2>
               <p>{howtoprepare}</p>
             </div>
