5041cdf6cfe4b65079893863d1dc19c228b45245
Letizia Fazzini <letizia.fazzini.tx@gmail.com>
2022-08-24 23:41:10 -0500

    New officer pictures 2022-23

--- src/Team.js
+++ src/Team.js
@@ -5,15 +5,19 @@ export default class Team extends Component {
   render() {
     // all officer photos should be square aspect ratio
     const officers = [
-      { name: "Katherine", position: "Co-President", url: "images/officers/katherine.jpg" },
-      { name: "Michelle", position: "Co-President", url: "images/officers/michelle.jpg" },
-      { name: "Bronson", position: "Events", url: "images/officers/bronsonz.jpg" },
-        { name: "Faiza", position: "Events", url: "images/officers/faiza.jpg" },
-      { name: "Siddhi", position: "Operations", url: "images/officers/siddhi.jpg" },
-      { name: "Preeti", position: "Marketing", url: "images/officers/preeti.jpg" },
-        { name: "David", position: "Curriculum", url: "images/officers/david.jpg" },
-        { name: "Proma", position: "Finance", url: "images/officers/proma.jpg" },
-      { name: "Bill Bulko", position: "Faculty Advisor", url: "images/officers/bulko.jpg" },
+      { name: "David", position: "President", url: "images/officers/david.jpg"},
+      { name: "Faiza", position: "Vice President", url: "images/officers/faiza.jpg"},
+      { name: "Proma", position: "Finance", url: "images/officers/proma.jpg"},
+      { name: "Allen", position: "Events", url: "images/officers/allen.jpg"},
+      { name: "Shruti", position: "Events", url: "images/officers/shruti.jpg"},
+      { name: "Letizia", position: "Tech", url: "images/officers/letizia.jpg"},
+      { name: "Kunal", position: "Curriculum", url: "images/officers/kunal.jpg"},
+      { name: "Asim", position: "Curriculum", url: "images/officers/asim.jpg"},
+      { name: "Peyton", position: "Operations", url: "images/officers/peyton.jpg"},
+      { name: "Varsha", position: "Marketing", url: "images/officers/varsha.jpg"},
+      { name: "Vibhuti", position: "Recruitment", url: "images/officers/vibhuti.jpg"},
+      { name: "Jake", position: "Recruitment", url: "images/officers/jake.jpg"},
+      { name: "Bill Bulko", position: "Faculty Advisor", url: "images/officers/bulko.jpg"},
     ];
 
     return (
